0167ac1e02050f5bd3cc971d7e4e7cabbae01669
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2011-04-24 13:59:16 +0200

    adding xlw code + generating output in lib and bin directories

---
+++ app/mgxll/cpp_iface/justatest.cpp
@@ -0,0 +1,10 @@
+#include "mgxll/cpp_iface/justatest.h"
+
+
+using namespace std;
+
+
+short JustATest()
+{
+	return 5;
+}
---
+++ app/mgxll/cpp_iface/justatest.h
@@ -0,0 +1,20 @@
+#ifndef __JUST_A_TEST_H__
+#define __JUST_A_TEST_H__
+
+
+#include <xlw/MyContainers.h>
+#include <xlw/CellMatrix.h>
+#include <xlw/DoubleOrNothing.h>
+#include <xlw/ArgList.h>
+#include <xlw/Wrapper.h>
+
+
+using namespace xlw;
+
+
+//<xlw:libraryname=MyTestLibrary
+short // just a test
+JustATest();
+
+
+#endif
